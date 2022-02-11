2e7576861568845d0698c74ac0176c06a058c3e7
Josh <thejosh2021@gmailcom>
2022-02-11 16:22:34 -0500

    added square exercise

---
+++ Unit 3 Collections/SquareRedo/SquareRedo/Program.cs
@@ -0,0 +1,24 @@
+﻿using SquareExercise;
+
+Square square = new Square();
+
+Console.WriteLine("Please enter a side length");
+
+int side;
+
+while(!int.TryParse(Console.ReadLine(), out side))
+{
+    Console.WriteLine("Not a valid side length, please enter again: ");
+}
+
+// int side = Int32.Parse(Console.ReadLine());
+
+square.SideLength = side;
+
+Console.WriteLine($"The perimeter of the square is {square.CalculatePerimeter()}");
+
+Console.WriteLine();
+
+Console.WriteLine($"The are of the square is {square.CalculateArea()}");
+
+
---
+++ Unit 3 Collections/SquareRedo/SquareRedo/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareExercise
+{
+    internal class Square
+    {
+        public int SideLength { get; set; }
+
+        // Methods
+
+        public int CalculateArea()
+        {
+            return SideLength * SideLength;
+        }
+
+        public int CalculatePerimeter()
+        {
+            return 4 * SideLength;
+        }
+    }
+}
+
